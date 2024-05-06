0a8240631b653b995f27380339aab453fe4f63e7
Macmac331 <72446027+Macmac331@users.noreply.github.com>
2024-05-06 20:11:59 +0800

    Modified transcript summarizer

--- speechify-be/analyzer/Summarizer.py
+++ speechify-be/analyzer/Summarizer.py
@@ -1,25 +1,20 @@
-import math
-from transformers import T5ForConditionalGeneration, T5Tokenizer
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
+
 
 class Summarizer:
-    @staticmethod
-    def transcriptSummarizer(text):
-        # Getting words count
-        words_count = text.split()
-        total_words = len(words_count)
 
-        min_length = max(math.ceil(total_words * 0.3), total_words // 10)
-        max_length = max(math.ceil(total_words * 0.6), total_words * 5 // 10)
+    def transcript_summarizer(transcript):
 
-        model_name = "t5-base"
-        model = T5ForConditionalGeneration.from_pretrained(model_name)
-        tokenizer = T5Tokenizer.from_pretrained(model_name)
+        genai.configure(
+            api_key=os.getenv('API_KEY')
+        )
+        model = genai.GenerativeModel('gemini-pro')
+        chat = model.start_chat(history=[])
 
-        input_text = "summarize: " + text
+        prompt = "Summarize: " + transcript
+        response = chat.send_message(prompt)
 
-        input_ids = tokenizer.encode(input_text, return_tensors="pt")
-        outputs = model.generate(input_ids, max_length=max_length, min_length=min_length, num_beams=4, early_stopping=True)
-        
-        summary_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-        
-        return summary_text
+        return response.text
